92ded00efe8be5ac5839361a8d2083ea5097e2ea
sandtechnology <a1294790523@hotmail.com>
2022-12-01 12:43:19 +0800

    Initial commit

---
+++ src/main/java/com/github/sandtechnology/fixds_store/FixDS_Store.java
@@ -0,0 +1,32 @@
+package com.github.sandtechnology.fixds_store;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("fixds_store")
+public class FixDS_Store {
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public FixDS_Store() {
+
+    }
+
+
+}
---
+++ src/main/java/com/github/sandtechnology/fixds_store/mixin/MixinPathResourcePack.java
@@ -0,0 +1,15 @@
+package com.github.sandtechnology.fixds_store.mixin;
+
+@org.spongepowered.asm.mixin.Mixin(net.minecraftforge.resource.PathResourcePack.class)
+public class MixinPathResourcePack {
+
+    @org.spongepowered.asm.mixin.injection.Redirect(method = "getResources",at = @org.spongepowered.asm.mixin.injection.At(value = "INVOKE", target = "Ljava/nio/file/Files;walk(Ljava/nio/file/Path;[Ljava/nio/file/FileVisitOption;)Ljava/util/stream/Stream;"))
+    public java.util.stream.Stream<java.nio.file.Path> getResources(java.nio.file.Path start, java.nio.file.FileVisitOption[] options)
+    {
+        try {
+            return java.nio.file.Files.walk(start,options).filter(java.nio.file.Files::isDirectory);
+        } catch (java.io.IOException e) {
+            return java.util.stream.Stream.empty();
+        }
+    }
+}
