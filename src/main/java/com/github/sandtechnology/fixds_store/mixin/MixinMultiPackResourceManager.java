fe1a0725b65e1ce31d1fdc49ee34f46e3c396add
sandtechnology <a1294790523@hotmail.com>
2022-12-01 21:44:05 +0800

    Cleanup unused things

--- src/main/java/com/github/sandtechnology/fixds_store/mixin/MixinMultiPackResourceManager.java
+++ src/main/java/com/github/sandtechnology/fixds_store/mixin/MixinMultiPackResourceManager.java
@@ -5,12 +5,7 @@ public class MixinMultiPackResourceManager {
     private final org.slf4j.Logger logger = com.mojang.logging.LogUtils.getLogger();
     @org.spongepowered.asm.mixin.Final
     @org.spongepowered.asm.mixin.Shadow
-    @org.spongepowered.asm.mixin.Mutable
     private java.util.Map<String, net.minecraft.server.packs.resources.FallbackResourceManager> namespacedManagers;
-    @org.spongepowered.asm.mixin.Final
-    @org.spongepowered.asm.mixin.Shadow
-    @org.spongepowered.asm.mixin.Mutable
-    private java.util.List<net.minecraft.server.packs.PackResources> packs;
 
     @org.spongepowered.asm.mixin.injection.Inject(method = "<init>", at = @org.spongepowered.asm.mixin.injection.At(value = "TAIL"))
     public void onInit(net.minecraft.server.packs.PackType p_203797_, java.util.List p_203798_, org.spongepowered.asm.mixin.injection.callback.CallbackInfo ci) {
